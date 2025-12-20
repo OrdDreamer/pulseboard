5edad16acdc71f5c2d4f8a48ebac7c7a78fe259a
Roman Snitsarenko <roman.snitsarenko@gmail.com>
2025-12-20 18:05:29 +0100

    Add environment-specific settings for development and production; configure dotenv for environment variables

--- pulseboard/settings/base.py
+++ pulseboard/settings/base.py
@@ -12,25 +12,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure--nn*&3=h73fb0t=20!&6v@p6of(ysag_9f)d^(&2&g&$frhdp&")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost"
-]
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure--nn*&3=h73fb0t=20!&6v@p6of(ysag_9f)d^(&2&g&$frhdp&")
 
 
 # Application definition
@@ -77,16 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "pulseboard.wsgi.application"
 
 AUTH_USER_MODEL = "core.Worker"
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
